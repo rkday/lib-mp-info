7b1b6ca04aa36a109d002a2a36a872ffedb97b40
Rob Day <robertkday@gmail.com>
2012-11-19 23:18:21 +0000

    Updating version to 0.2.0

--- MPInfo/setup.py
+++ MPInfo/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='MPInfo',
-    version='0.1.0',
+    version='0.2.0',
     author='Robert Day',
     author_email='rkd@rkd.me.uk',
     packages=['mpinfo'],
